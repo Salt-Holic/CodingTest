58d1205b72f0eb0b758bb591f4b3b85dfdc6e446
jh868 <2weeks9947@naver.com>
2022-12-13 21:56:20 +0900

    BAEKJOON

---
+++ CodingTest_BAEKJOON/ch.15_정수론 및 조합론/1934.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
+for i in range(n):
+    a, b = map(int, input().split())
+    print(lcm(a, b))
